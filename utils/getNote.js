f52cea0decdc517cd40394ba2d8ff1b3e029b04e
satwikpadhy <satwikpadhy@gmail.com>
2024-05-21 22:16:16 +0530

    Added support for other file types and fixed "note does not exist"

--- utils/getNote.js
+++ utils/getNote.js
@@ -18,15 +18,41 @@ const getNote = function(bot,connString,chatId,spl) {
             })
             .then((result) => {
                 console.log('Query result:', result.rows);
-                fileId = result.rows[0].data
-                type = result.rows[0].type
-                if(type == "img") {
-                    bot.sendPhoto(chatId,fileId)
+                console.log('Result rows length :', result.rows.length)
+                if(result.rows.length == 0) {
+                    bot.sendMessage(chatId,"Note does not exist!")
                 }
+                else {
+                    fileId = result.rows[0].data
+                    type = result.rows[0].type
+                    if(type == "img") {
+                        const options = {caption:`Here's the image named → ${noteName}`}
+                        bot.sendPhoto(chatId,fileId,options)
+                    }
+                    else if(type == "txt") {
+                        bot.sendMessage(chatId,`Here's the note named → ${noteName} : \n\n${fileId}`)
+                    }
+                    else if(type == "vid") {
+                        const options = {caption:`Here's the video named → ${noteName}`}
+                        bot.sendVideo(chatId,fileId,options)
+                    }
+                    else if(type == "doc") {
+                        const options = {caption:`Here's the document named → ${noteName}`}
+                        bot.sendDocument(chatId,fileId,options)
+                    }
+                    else if(type == "aud") {
+                        const options = {caption:`Here's the audio named → ${noteName}`}
+                        bot.sendAudio(chatId,fileId,options)
+                    }
+                    else if(type == "voice") {
+                        const options = {caption:`Here's the voice named → ${noteName}`}
+                        bot.sendVoice(chatId,fileId,options)
+                    }
+                }  
                 // Similarly add other cases for other file types and handle note not found
             })
             .catch((error) => {
-                console.error('Error executing query:', error);
+                console.error('Error executing the "get" query:', error);
             })
             .finally(() => {
                 
