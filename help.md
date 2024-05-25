eb58ff594ac9a711f2776adc915ba01be1449311
satwikpadhy <satwikpadhy@gmail.com>
2024-05-26 00:22:46 +0530

    Added /help and made the username in /start dynamic

---
+++ help.md
@@ -0,0 +1,7 @@
+Hi user! I am @${me}, Thanks for the interest in this bot!
+
+Here are the currently supported commands:
+1. /save : syntax => /save notename : save a message as a note by replying to it.
+2. /notes : list all the saved notes in the chat
+3. /get : syntax => /get notename : retrieve an existing note
+4. /delete : syntax => /delete notename : delete an existing note
--- index.js
+++ index.js
@@ -4,26 +4,26 @@ const notes = require('./utils/notes')
 const deleteNote = require('./utils/deleteNote')
 require('dotenv').config()
 const TelegramBot = require('node-telegram-bot-api');
+const fs = require('fs')
 
 const token = process.env.token
 
 const bot = new TelegramBot(token, {polling: true})
-
 const connString = {
-  database : process.env.database,
-  user : process.env.user,
-  password : process.env.password,
-  host : process.env.host,
-  port : process.env.port
+    database : process.env.database,
+    user : process.env.user,
+    password : process.env.password,
+    host : process.env.host,
+    port : process.env.port
 }
 
 encryptionKey = process.env.key
 var me = ''
 const getMe = (async () => {
     me = await bot.getMe()
-    console.log(me.username)
 })
 getMe()
+const helpText = fs.readFileSync('help.md').toString()
 
 bot.on('message', (msg) => {
   const chatId = msg.chat.id
@@ -34,13 +34,11 @@ bot.on('message', (msg) => {
     command = spl[0]
 
     if(command == "/start") {
-        reply_text = "Hello I am @cruzex_bot. Send /help to get a list of commands."
-        bot.sendMessage(chatId, reply_text)
+        bot.sendMessage(chatId, `Hello I am @${me.username}. Send /help to get a list of commands.`)
     }
 
     else if(command == "/help" || command ==`/help@${me.username}`) {
-        reply_text = "Help functionality yet to be implemented."
-        bot.sendMessage(bot,chatId, reply_text)
+        bot.sendMessage(chatId, helpText.replace("${me}", `${me.username}`))
     }
 
     else if(command == "/save" || command == `/save@${me.username}`) {
